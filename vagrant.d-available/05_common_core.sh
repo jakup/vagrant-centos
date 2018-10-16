9a0acedfa94acc2990b1b8b9123ed4d0bd7c0136
Jake Kupersmith <jkupersmith@mynetwatchman.com>
2018-10-15 23:59:34 -0400

    add /usr/local/bin to path

--- vagrant.d-available/05_common_core.sh
+++ vagrant.d-available/05_common_core.sh
@@ -1,3 +1,4 @@
 # common system packages
 yum -y install htop strace lsof policycoreutils-python git bind-utils nmap-ncat tcpdump zip
 
+echo 'export PATH="$PATH:/usr/local/bin"' > /etc/profile.d/usr_local_bin.sh
