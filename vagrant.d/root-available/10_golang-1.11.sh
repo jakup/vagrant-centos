5dc64b9993c99549dea8e99ff8f983bf7be90092
Jake Kupersmith <jkupersmith@mynetwatchman.com>
2018-11-28 21:53:54 -0500

    golang 1.11.2

--- vagrant.d/root-available/10_golang-1.11.sh
+++ vagrant.d/root-available/10_golang-1.11.sh
@@ -1,4 +1,4 @@
-curl https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+curl https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz | tar -C /usr/local -xzf -
 (cat <<'EOF'
 PATH="$PATH:/usr/local/go/bin:$HOME/go/bin"
 export PATH
